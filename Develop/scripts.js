53bc4f381845377d8737bc819f3f31a2da40c83f
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-04-07 22:07:56 -0400

    update Css and js logic for local storage

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,20 +1,32 @@
 // define a variable for today as moment
 // eg. var today = moment();
-var currentTime = moment();
-console.log(currentTime.format("LTS"));
+
+var currentDate = moment();
+
+$('#currentDay').text(currentDate.format('dddd, MMMM Do'))
+
 //define the variable of start time of the day
 var startTimeOfTheDay = 09;
 
 // define start of the day
-var start = currentTime.clone().set("hour", startTimeOfTheDay);
+var start = currentDate.clone().set("hour", startTimeOfTheDay);
 
 // define end of the day
-var end = currentTime.clone().set("hour", 17);
+var end = currentDate.clone().set("hour", 17);
 // define the difference between start and end in number of hours
 var duration = moment.duration(end.diff(start));
 var hours = duration.asHours();
 // loop over number of hours
 // render html
+const dateInLocalStorage = localStorage.getItem("scheduleDate");
+localStorage.setItem("scheduleDate", currentDate.format("YYYY/MM/DD"));
+
+if (dateInLocalStorage != null) {
+  if (dateInLocalStorage != currentDate.format("YYYY/MM/DD")) {
+    localStorage.removeItem("schedule");
+  }
+}
+
 const schedule = JSON.parse(localStorage.getItem("schedule"));
 if (schedule == null) {
   let schedulerStorage = [];
@@ -23,28 +35,28 @@ if (schedule == null) {
   }
   localStorage.setItem("schedule", JSON.stringify(schedulerStorage));
 }
+const scheduleStorage = JSON.parse(localStorage.getItem("schedule"));
 for (let i = 0; i <= hours; i++) {
-    const scheduleStorage = JSON.parse(localStorage.getItem("schedule"));
-  const timeOfBlock = currentTime.clone().set("hour", startTimeOfTheDay + i);
+  const timeOfBlock = currentDate.clone().set("hour", startTimeOfTheDay + i);
   let currentState = "";
-  if (currentTime.isAfter(timeOfBlock)) {
+  if (currentDate.isAfter(timeOfBlock)) {
     currentState = "past";
   }
-  if (currentTime.isBefore(timeOfBlock)) {
+  if (currentDate.isBefore(timeOfBlock)) {
     currentState = "future";
   }
   if (
-    currentTime.isSameOrAfter(timeOfBlock) &&
-    currentTime.isBefore(timeOfBlock.clone().add(1, "hours"))
+    currentDate.isSameOrAfter(timeOfBlock) &&
+    currentDate.isBefore(timeOfBlock.clone().add(1, "hours"))
   ) {
     currentState = "present";
   }
-  const $divTimeBlock = $("<div>", { id: "test", class: "row time-block" });
-  const $divHour = $("<div>", { class: "col-md-1 hour" });
+  const $divTimeBlock = $("<div>", { class: "row time-block" });
+  const $divHour = $("<div>", { class: "col-md-2 hour" });
   const $textArea = $("<textarea>", {
-    class: "col-md-10 description " + currentState,
+    class: "col-md-9 description " + currentState,
   });
-  
+
   $textArea.val(scheduleStorage[i].value);
   const $icon = $("<i>", { class: "fas fa-save" });
   const $button = $("<button>", { class: "btn saveBtn col-md-1" });
